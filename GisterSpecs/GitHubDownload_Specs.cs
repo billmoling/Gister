843342e75e35e83bfa0ce6310fe40abe51a7c86d
Ling <panan.songyu@gmail.com>
2012-12-23 17:19:38 +0800

    add the test class, need to change the user/pw for debugging

---
+++ GisterSpecs/GitHubDownload_Specs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EchelonTouchInc.Gister.Api;
+using EchelonTouchInc.Gister.Api.Credentials;
+using NUnit.Framework;
+
+namespace GisterSpecs
+{
+    [TestFixture]
+    public class GitHubDownload_Specs
+    {
+        
+
+        [Test]
+        public void DownloadGistList()
+        {
+            ProcessGistList obj = new ProcessGistList();
+            GitHubUserCredentials mycred=new GitHubUserCredentials ("XXXXX","XXXX");
+            obj.GetUserGistList(mycred);
+
+        }
+    }
+}
